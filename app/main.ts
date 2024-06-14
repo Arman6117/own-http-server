e900b3508a357a79050d24cc1e9088c4f624e77b
Arman Patel <vanixgaming634@gmail.com>
2024-06-14 21:13:13 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -7,28 +7,8 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-
-    const headers = request.split("\r\n")[2];
-    const userAgent = headers.split(": ");
-
-    const path = request.split(" ")[1];
-    if (path === "/") {
-      const response = `HTTP/1.1 200 OK\r\n\r\n`;
-      socket.write(response);
-    } else if (path.startsWith("/echo/")) {
-      const message = path.slice(6);
-      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
-      socket.write(response);
-    } else if (userAgent) {
-      const message = userAgent[1];
-
-      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`;
-
-      socket.write(response);
-    } else {
-      const response = `HTTP/1.1 404 Not Found\r\n\r\n`;
-      socket.write(response);
-    }
+    const path = request.split(' ')[1]
+    console.log(path.split('/'))
   });
 });
 
