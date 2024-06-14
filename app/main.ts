9f6c9738b8dc931ae05403ae59abb6e08ceff0ae
Arman Patel <vanixgaming634@gmail.com>
2024-06-14 20:32:06 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -11,7 +11,7 @@ const server = net.createServer((socket) => {
     const path = request.split(' ')[1]
 
     if (path === '/') {
-      const response = `HTTP/1.1 200 OK\r\n`
+      const response = `HTTP/1.1 200 OK\r\n\r\n`
       socket.write(response)
     }else if(path === '/echo') {
 
@@ -20,7 +20,7 @@ const server = net.createServer((socket) => {
        socket.write(response)
 
     }else {
-      const response = `HTTP/1.1 404 Not Found\r\n`
+      const response = `HTTP/1.1 404 Not Found\r\n\r\n`
       socket.write(response)
     }
  
