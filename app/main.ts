7fc7323f44627287f0cb5a9eae94216a47f4d96c
Arman Patel <vanixgaming634@gmail.com>
2024-06-14 21:24:45 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -7,34 +7,37 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-    const path = request.split(' ')[1]
-    const params =  path.split('/')[1]
+    const path = request.split(" ")[1];
+    const params = path.split("/")[1];
 
-    let response:string;
+    let response: string;
 
-    const changeResponse = (response:string) =>{
-      socket.write(response)
+    const changeResponse = (response: string) => {
+      socket.write(response);
       socket.end();
-    }
+    };
 
     switch (params) {
-      case '' : 
-        response = `HTTP/1.1 200 OK\r\n\r\n`
+      case "":
+        response = `HTTP/1.1 200 OK\r\n\r\n`;
         changeResponse(response);
         break;
-      case 'echo' : 
-      const message = path.slice(6)
-        response = `HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:${message.length}\r\n\r\n${message}`
+      case "echo":
+        const message = path.slice(6);
+        response = `HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:${message.length}\r\n\r\n${message}`;
         changeResponse(response);
         break;
-      case 'user-agent' : 
-      const userAgent = request.split('\r\n')[2]
-      const userAgentMessage = userAgent.split(": ")[1];
-      console.log(userAgentMessage)
-      //   response = `HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:${message.length}\r\n\r\n${message}`
-      //   changeResponse(response);
+      case "user-agent":
+        const userAgent = request.split("\r\n")[2];
+        const userAgentMessage = userAgent.split(": ")[1];
+        console.log(userAgentMessage);
+        response = `HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:${userAgentMessage.length}\r\n\r\n${userAgentMessage}`;
+        changeResponse(response);
         break;
-    } 
+
+      default : 
+        response = `HTTP/1.1 404 Not Found\r\n\r\n`
+    }
   });
 });
 
