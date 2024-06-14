2db155b9e9728e32dd40d09e749d57dc67cc7ef6
Arman Patel <vanixgaming634@gmail.com>
2024-06-14 19:34:32 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,7 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
- socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`))
+ socket.write(Buffer.from(`HTTP/1.1 404 Not Found OK\r\n\r\n`))
 });
 
 server.listen(4221, "localhost");
