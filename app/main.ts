3d1a8ffdee014adb312ba07f1fddc924eb548181
Arman Patel <vanixgaming634@gmail.com>
2024-06-14 20:13:20 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -5,16 +5,17 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-//  socket.on('data', (data)=> {
-//     const request = data.toString();
-//     const pathname = request.split(' ')[1]
-//     const response = pathname === '/' ? `HTTP/1.1 200 OK\r\n\r\n` : `HTTP/1.1 404 Not Found\r\n\r\n`
+ socket.on('data', (data)=> {
+    const request = data.toString();
+    const headersAndBody = request.split('\r\n\r\n')
+    const headers = headersAndBody[0].split('\r\n');
+    const body = headersAndBody[1]
+  
+    console.log(body)
+   //  socket.write(response)
+ })
 
-//     socket.write(response)
-//  })
 
-const responseContent = `HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:9\r\n\r\nraspberry` 
-socket.write(responseContent)
 });
 
 server.listen(4221, "localhost");
