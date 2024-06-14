9b9170abb9a49381c77d97911236c7271b3113df
Arman Patel <vanixgaming634@gmail.com>
2024-06-14 20:47:29 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -14,7 +14,7 @@ const server = net.createServer((socket) => {
 
     if(userAgent) {
       const message = userAgent[1];
-      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length-1}\r\n\r\n${message}`
+      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`
 
       socket.write(response)
     }
