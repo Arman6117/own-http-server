8397e3077010eb545bba17bd5cce390256586c2f
Arman Patel <vanixgaming634@gmail.com>
2024-06-14 21:22:12 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -8,7 +8,32 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     const path = request.split(' ')[1]
-    console.log(path.split('/'))
+    const params =  path.split('/')[1]
+
+    let response:string;
+
+    const changeResponse = (response:string) =>{
+      socket.write(response)
+      socket.end();
+    }
+
+    switch (params) {
+      case '' : 
+        response = `HTTP/1.1 200 OK\r\n\r\n`
+        changeResponse(response);
+        break;
+      case 'echo' : 
+      const message = path.slice(6)
+        response = `HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:${message.length}\r\n\r\n${message}`
+        changeResponse(response);
+        break;
+      case 'user-agent' : 
+      const userAgent = request.split('\r\n')[2]
+      console.log(userAgent)
+      //   response = `HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:${message.length}\r\n\r\n${message}`
+      //   changeResponse(response);
+        break;
+    } 
   });
 });
 
