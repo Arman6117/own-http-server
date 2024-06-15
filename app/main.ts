25a635a1af37995a86a3212ad005abf243b122a1
Arman Patel <vanixgaming634@gmail.com>
2024-06-15 15:33:31 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -34,6 +34,7 @@ const server = net.createServer((socket) => {
             const [____, encodingType] = requestLines[2].split(": ");
 
            
+            console.log(encodingType)
             if (encodingType === 'gzip') {
               response = `HTTP/1.1 200 OK\r\nContent-Encoding:${encodingType}\r\nContent-Type:text/plain\r\nContent-Length:${message.length}\r\n\r\n${message}`;
               changeResponse(response);
