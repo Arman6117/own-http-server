bf204009e95f1a88d8bfb0e83fea855527dd7077
Arman Patel <vanixgaming634@gmail.com>
2024-06-15 15:30:02 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -33,6 +33,7 @@ const server = net.createServer((socket) => {
 
             const [____, encodingType] = requestLines[2].split(": ");
 
+            console.log(encodingType)
             if (encodingType !== "invalid-encoding") {
               response = `HTTP/1.1 200 OK\r\nContent-Encoding:${encodingType}\r\nContent-Type:text/plain\r\nContent-Length:${message.length}\r\n\r\n${message}`;
               changeResponse(response);
