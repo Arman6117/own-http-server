35cb02ebe6c82adbea2832ba0636160648eb95f9
Arman Patel <vanixgaming634@gmail.com>
2024-06-15 13:05:29 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -67,7 +67,7 @@ const server = net.createServer((socket) => {
         const [_,dirName,fileName] = path.split('/')
         const filePath = dirName + "/" + fileName;
 
-        const body = requestLines[1]
+        const body = requestLines
         console.log("logged body" + body)
     }
   });
