b49af6b420399bb7b68e87ad66a53b26b7e3eac4
Arman Patel <vanixgaming634@gmail.com>
2024-06-15 14:03:13 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -30,7 +30,8 @@ const server = net.createServer((socket) => {
             break;
           case "echo":
             const message = path.slice(6);
-            console.log()
+
+            console.log(requestLines)
             response = `HTTP/1.1 200 OK\r\nContent-Encoding:gZip\r\nContent-Type:text/plain\r\nContent-Length:${message.length}\r\n\r\n${message}`;
             changeResponse(response);
             break;
